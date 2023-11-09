d8d0a5624d674c9359bfd7a1fc8ed00bc598b642
Faniry <faniryrandria08@gmail.com>
2023-11-09 08:47:44 +0300

    reporting to git the directory

---
+++ drugStore/src/test/java/drugstore/fn/drugstore/DrugStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package drugstore.fn.drugstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrugStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
